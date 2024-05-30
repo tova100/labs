b430e2b9ff77303a13721b35f98c2797ca90c60e
tova100 <tova0556768145@gmail.com>
2024-05-30 15:42:45 +0300

    lending after changes

--- src/Lending/lending.sol
+++ src/Lending/lending.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./Math.sol";
+import "./mathLending.sol";
 
 interface ILendingPool {
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
@@ -17,22 +17,29 @@ interface IWETHGateway {
 }
 
 contract Lending {
+    using Mathl for uint256;
+
     ILendingPool public constant aave = ILendingPool(0x56Ab717d882F7A8d4a3C2b191707322c5Cc70db8);
     IWETHGateway public constant wethGateway = IWETHGateway(0x2Fa2e7a6dEB7bb51B625336DBe1dA23511914a8A);
     mapping(address => uint256) borrowedUsers;
     mapping(address => uint256) collateralUsers;
+    uint256 public baseRate = 20000000000000000;
+    uint256 public borrowRate = 300000000000000000;
+
     uint256 public totalBorrowed;
     uint256 public totalReserve;
     uint256 public totalDeposit;
     uint256 public totalCollateral;
     IERC20 DAI;
+    
     IERC20 tokenDai;
     IERC20 tokenETH;
+    uint256 public maxLTV = 4;
 
     receive() external payable {}
     //מחזיר את הערך הנוכחי של HTE
 
-    function _getLatestPrice() public view returns (int256) {
+    function getLatestPrice() public view returns (int256) {
         (, int256 price,,,) = priceFeed.latestRoundData();
         return price * 10 ** 10;
     }
@@ -74,7 +81,7 @@ contract Lending {
         DAI.transferFrom(msg.sender, address(this), Dai);
         sendDaiToAave(Dai);
         totalDeposit += Dai;
-        uint256 tokenDaiAmount = getExp(amount, getExchangeRate());
+        uint256 tokenDaiAmount = getExp(Dai, getExchangeRate());
 
         //transfer to user token dai
         tokenDai.mint(msg.sender, tokenDaiAmount); //מנפיקה טוקנים שיהיה לי להחזיר למי שמפקיד דאי
@@ -82,9 +89,9 @@ contract Lending {
     //withdraw dai
 
     function withdrawDai(uint256 amount) public isCorrect(amount) {
-        require(amount <= balanceOf(msg.sender), "Not enough Token!");
+        require(amount <= msg.sender.balanceOf(msg.sender), "Not enough Token!");
         DAI.transferFrom(address(this), msg.sender, amount);
-        uint256 daiToReceive = mulExp(_amount, getExchangeRate());
+        uint256 daiToReceive = mulExp(amount, getExchangeRate());
         tokenDai.burn(amount);
         withdrawDaiFromAave(amount);
         totalDeposit -= daiToReceive;
@@ -94,13 +101,13 @@ contract Lending {
         //get ETH and transfer DAI
         totalCollateral += amount;
         collateralUsers[msg.sender] += amount;
-        _sendWethToAave(_amount);
+        _sendWethToAave(amount);
     }
 
     function removeCollateral(uint256 amount) public isCorrect(amount) {
         // remove ETH
         //אתריום ששוה עכשיו בשוק
-        uint256 wethPrice = uint256(_getLatestPrice());
+        uint256 wethPrice = uint256(getLatestPrice());
         //כמות הביטחונות של משתמש ספציפי
         uint256 collateral = collateralUsers[msg.sender];
         require(collateral >= amount, "cant take ETH ");
@@ -116,16 +123,15 @@ contract Lending {
         withdrawWethFromAave(amount);
         //explain
         payable(address(this)).transfer(amount);
-        payable(msg.sender).transfer(amount + interest);
     }
     //borrow
 
     function borrowDai(uint256 amount) public {
         require(collateralUsers[msg.sender] > 0, "you dont have any collateral");
-        uint256 borrow = borrowedUsers[msg.sender];
+        uint256 borrowed = borrowedUsers[msg.sender];
         uint256 collateral = collateralUsers[msg.sender];
         //בודק את הערך של האתריום היום בשוק ומכפיל בסך הביטחונות שיש למשתמש
-        uint256 left = mulExp(collateral, uint256(getLatestPrice())) - barrowed;
+        uint256 left = mulExp(collateral, uint256(getLatestPrice())) - borrowed;
         //מחזיר כמה יש אפשרות להלוואות
         uint256 borrowLimit = percentage(left, maxLTV);
         require(borrowLimit >= amount, "you cant borrow");
@@ -137,11 +143,25 @@ contract Lending {
 
     function repay(uint256 amount) public {
         require(borrowedUsers[msg.sender] > 0, "you dont have any debt to return ");
-
-        totalBorrowed -= amount;
-        borrowedUsers[msg.sender] -= amount;
+        uint256 ratio = getExp(totalBorrowed, totalDeposit);
+        uint256 interestMul = getExp(borrowRate - baseRate, ratio);
+        uint256 rate = mulExp(ratio, interestMul) + baseRate;
+        uint256 fee = amount * rate;
+        uint256 paid = amount - fee;
+        totalReserve += fee;
+        totalBorrowed -= paid;
+        borrowedUsers[msg.sender] -= paid;
+        sendDaiToAave(amount);
     }
-    
+
+    function triggerLiquidation(address user) public {
+        uint256 barrowed = borrowedUsers[user];
+        uint256 collateral = collateralUsers[user];
+        uint256 left = mulExp(collateral, getLatestPrice()) - barrowed;
+
+        percentage(left, maxLTV);
+    }
+
     function getExchangeRate() public view returns (uint256) {
         if (totalSupply() == 0) {
             return 1000000000000000000;
@@ -150,5 +170,4 @@ contract Lending {
         uint256 num = cash + (totalBorrowed) + (totalReserve);
         return getExp(num, totalSupply());
     }
-    
 }
