d221c3ff2c3d104475ed524149e4905f4a6f96da
tova100 <tova0556768145@gmail.com>
2024-06-04 10:09:23 +0300

    Solidity by Example

---
+++ src/Solidity by Example/ex.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20
+contract HelloWorld{
+    string public greet="hello world";
+}
+contract Counter{
+    uint256 public count;
+    function getCount()public view return uint256{
+     return count;   
+    }
+    function inc ()public {
+        return count+=1;
+    }
+    function dec ()public {
+        return count-=1;
+    }
+}
+contract Primitivim{
+   bool public boo = true;
+   uint8 public u8 =1;
+   uint256 public u256=456;
+}
+contract Variables{
+    uint256 public num=123;
+    string public text="Hello";
+    function doSamething()public {
+    uint i=456;
+    uint256 timestamp=block.timestamp;
+    address sender=msg.sender;
+    }
+}
+
