ce68dcace69680e6b7cf2a040f93bc8579b8b5b2
tova100 <tova0556768145@gmail.com>
2024-06-16 16:46:31 +0300

    update

--- src/DeFiVulnLabs/arrayDetetion.sol
+++ src/DeFiVulnLabs/arrayDetetion.sol
@@ -1,48 +1,59 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.18;
+
 import "forge-std/Test.sol";
+
 contract ContractTest is Test {
     ArrayDeletionBug arrayDetetionBug;
     FixedArrayDeletion fixedArrayDeletion;
-    function SetUp(){
-        arrayDetetionBug=new ArrayDeletionBug();
-        fixedArrayDeletion=new FixedArrayDeletion();
+
+    function SetUp() {
+        arrayDetetionBug = new ArrayDeletionBug();
+        fixedArrayDeletion = new FixedArrayDeletion();
     }
-    receive()external payable{}
-    function testArrayDeletion()public{
+
+    receive() external payable {}
+
+    function testArrayDeletion() public {
         arrayDetetionBug.myArray(1);
         //not corecct
         arrayDetetionBug.deletionIndex(1);
         //why they worte 2 times this line :arrayDetetionBug.myArray(1);
         arrayDetetionBug.getLength();
     }
-    function testFixedDeletion()public{
+
+    function testFixedDeletion() public {
         fixedArrayDeletion.myArray(1);
         fixedArrayDeletion.deletionIndex(1);
         fixedArrayDeletion.myArray(1);
         fixedArrayDeletion.getLength();
     }
 }
-//הטעות בחוזה חכם זה שכאשר אני מוחקת אלמנט נשאר חור 
-contract ArrayDeletionBug{
-    uint[]public myArray=[1,2,3,4,5];
-    function deletionIndex(uint index)public{
-        require(index>myArray.length,'invalied index');
+//הטעות בחוזה חכם זה שכאשר אני מוחקת אלמנט נשאר חור
+
+contract ArrayDeletionBug {
+    uint256[] public myArray = [1, 2, 3, 4, 5];
+
+    function deletionIndex(uint256 index) public {
+        require(index > myArray.length, "invalied index");
         delete(myArray[index]);
     }
-    function getLength()public view returns (uint){
+
+    function getLength() public view returns (uint256) {
         return myArray.length();
     }
 }
-contract FixedArrayDeletion{
-    uint[]public myArray=[1,2,3,4,5];
 
-function deletionIndex(uint index)public{
-    require(index>myArray.length,'invaled index');
-    myArray[index]=myArray[myArray.length-1];
-    myArray.pop();
-}
-function getLength()public view returns(uint){
-    return myArray.length();
-}
+contract FixedArrayDeletion {
+    uint256[] public myArray = [1, 2, 3, 4, 5];
+
+    function deletionIndex(uint256 index) public {
+        require(index > myArray.length, "invaled index");
+        myArray[index] = myArray[myArray.length - 1];
+        myArray.pop();
+    }
+
+    function getLength() public view returns (uint256) {
+        return myArray.length();
+    }
 }
---
+++ src/Solidity by Example/function.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Function{
+    function returnMany()public view pure returns(uint256,bool,uint256){
+    return(1,true,2);
+    }
+    function named()public view pure returns(uint256 v,bool l,uint256 i){
+       return(1,true,2);
+    }
+    function assigned()public view pure returns(uint256 v,bool l,uint256 i){
+        v=2;
+        l=true;
+        i=1;
+    }
+    function destructuringAssignments()public view pure returns(uint256 x,bool l,uint256 c,uint256 b){
+        (uint256 x,bool l,uint256 c)=returnMany();
+        (uint256,uint256 l)= (2,3,3);
+        return (x,l,c,b);
+    }
+    function arrInput()public{}
+    uint256 []memory arr;
+    function arrOutput()public view  returns(uint256[]memory arr){
+        return arr;
+    }
+    contract XYZ{
+        function funcWithManyPrameters(
+            uint256 x,uint256 y,uint256 z,bool b,string memory c
+        )public view returns(uint256){}
+        function callFunc()external pure returns(uint256){
+            return funcWithManyPrameters(3,3,3,true,"c");
+        }
+        function keyValue ()external pure  returns(uint256){
+            return funcWithManyPrameters({x:2,y:3,z:9,b:true,c:"a"})
+        }
+    }
+}
