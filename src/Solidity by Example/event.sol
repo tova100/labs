76f7b98845bdbe2628302cf118e3740109de4e9a
tova100 <tova0556768145@gmail.com>
2024-06-17 13:39:52 +0300

    update solidity by example - version

--- src/Solidity by Example/event.sol
+++ src/Solidity by Example/event.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
+
 contract Event{
     //print to console 
     event log(address indexed addr,string massege);
     event anotherLog();
     function test()public{
        emit log(msg.sender,"hello world");
-       emit log(msg.sender, "Hi for every one");
+       emit log(msg.sender, "Hi for every one!");
        emit anotherLog();
     }
 }
